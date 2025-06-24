3118b24a4f378aff47fff0f2a7eb0313ede898d8
Volodymyr Lubenchenko <vlad.lubenchenko@gmail.com>
2025-06-24 23:28:18 +0300

    Refactor MyService.scala for improved readability and formatting. Adjusted import statements and aligned variable declarations. Enhanced error handling formatting in request index operation and streamlined validation function.

--- src/main/scala/mylogic/MyService.scala
+++ src/main/scala/mylogic/MyService.scala
@@ -6,7 +6,15 @@ import cats.implicits._
 import mylogic.Validators.{ValidationError, _}
 import mymodel._
 import org.slf4j.LoggerFactory
-import mylogic.Validators.{ArrayElementError, ArrayLengthError, TargetValueError, ValidationError, validateArrayElements, validateArrayLength, validateTarget}
+import mylogic.Validators.{
+  ArrayElementError,
+  ArrayLengthError,
+  TargetValueError,
+  ValidationError,
+  validateArrayElements,
+  validateArrayLength,
+  validateTarget
+}
 
 import scala.collection.mutable.ListBuffer
 
@@ -16,9 +24,9 @@ class MyService {
 
   private val config = ConfigLoader.config
 
-  val defaultTarget = config.defaultTarget.toIntOption.getOrElse(42)
+  val defaultTarget        = config.defaultTarget.toIntOption.getOrElse(42)
   val maxRequestsPerMinute = config.maxRequestsPerMinute.toIntOption.getOrElse(5)
-  val timeWindowMillis = config.timeWindowMillis.toLongOption.getOrElse("6000".toLong)
+  val timeWindowMillis     = config.timeWindowMillis.toLongOption.getOrElse("6000".toLong)
 
   val rateLimiter = RateLimiter(maxRequestsPerMinute, timeWindowMillis)
 
@@ -33,14 +41,17 @@ class MyService {
 
       if (errors.isInvalid) {
         val errorStr = errors.fold(
-          errors => errors.map {
-            case ArrayLengthError(actual, min, max) =>
-              s"Array length must be between $min and $max, but was $actual"
-            case ArrayElementError(value, min, max) =>
-              s"Array contains invalid element $value. Values must be between $min and $max"
-            case TargetValueError(value, min, max) =>
-              s"Target value $value is invalid. Must be between $min and $max"
-          }.mkString("\n"),
+          errors =>
+            errors
+              .map {
+                case ArrayLengthError(actual, min, max) =>
+                  s"Array length must be between $min and $max, but was $actual"
+                case ArrayElementError(value, min, max) =>
+                  s"Array contains invalid element $value. Values must be between $min and $max"
+                case TargetValueError(value, min, max) =>
+                  s"Target value $value is invalid. Must be between $min and $max"
+              }
+              .mkString("\n"),
           _ => "" // This won't be used since we're checking isInvalid
         )
         logger.error(s"Errors during request index operation: $errorStr")
@@ -49,12 +60,11 @@ class MyService {
       } else {
         val data = reqIdx.data
 
-        val resp = ResponseIndex(
-          data.indices.flatMap { i =>
-            (i + 1 until data.length)
-              .filter(j => data(i) + data(j) == reqIdx.target)
-              .map(j => Pair((i, j), (data(i), data(j))))
-          }.toList)
+        val resp = ResponseIndex(data.indices.flatMap { i =>
+          (i + 1 until data.length)
+            .filter(j => data(i) + data(j) == reqIdx.target)
+            .map(j => Pair((i, j), (data(i), data(j))))
+        }.toList)
 
         addToResults(reqIdx, resp)
         logger.info(s"Result of processing request index operation: ${resp.toString}")
@@ -80,11 +90,13 @@ class MyService {
 
       if (rateLimiter.isAllowed(clientId)) {
 
-        val filtRes = results.filter(v => v._1 match {
-          case ri: RequestIndex => ri.data.exists(d => req.data == d) ||
-            req.target.map(t => t == ri.target).getOrElse(false)
-          case rt: RequestTarget => rt.target == req.target
-        }
+        val filtRes = results.filter(v =>
+          v._1 match {
+            case ri: RequestIndex =>
+              ri.data.exists(d => req.data == d) ||
+              req.target.map(t => t == ri.target).getOrElse(false)
+            case rt: RequestTarget => rt.target == req.target
+          }
         )
         val res = ResponseFind(filtRes.toList.map(_._2))
         logger.info(s"Result of find opertion: ${res.toString}")
@@ -97,18 +109,16 @@ class MyService {
 
     }
 
-
   private def addToResults(req: MyRequest, resp: MyResponse): Unit =
     results += Tuple2(req, resp)
 
-  private def validateReqIdx(reqIdx: RequestIndex): Validated[List[ValidationError], RequestIndex] = {
+  private def validateReqIdx(reqIdx: RequestIndex): Validated[List[ValidationError], RequestIndex] =
     (
-      validateArrayLength(reqIdx.data).toValidatedNel, // Convert to ValidatedNel
+      validateArrayLength(reqIdx.data).toValidatedNel,   // Convert to ValidatedNel
       validateArrayElements(reqIdx.data).toValidatedNel, // Convert to ValidatedNel
-      validateTarget(reqIdx.target).toValidatedNel // Convert to ValidatedNel
-    ).mapN { (_, _, _) => reqIdx } // Combine validations
-      .leftMap(_.toList) // Convert NonEmptyList to List
-  }
+      validateTarget(reqIdx.target).toValidatedNel       // Convert to ValidatedNel
+    ).mapN((_, _, _) => reqIdx)                          // Combine validations
+      .leftMap(_.toList)                                 // Convert NonEmptyList to List
 
 }
 
